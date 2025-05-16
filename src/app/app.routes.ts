544ddf3e74b6891b937d9da5132dd58c4dfae2ee
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-17 00:51:41 +0300

    Folder Strucutre Bug Fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomepageComponent,
   },
 ];
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { HeroComponent } from '../../features/homepage/hero/hero.component';
+import { AboutUsComponent } from '../../features/homepage/about-us/about-us.component';
+import { OurServicesComponent } from '../../features/homepage/our-services/our-services.component';
+import { MarqueeComponent } from '../../features/homepage/marquee/marquee.component';
+import { ProjectsComponent } from '../../features/homepage/projects/projects.component';
+import { TestimonialsComponent } from '../../features/homepage/testimonials/testimonials.component';
+import { CtaComponent } from '../../features/homepage/cta/cta.component';
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [
+    HeroComponent,
+    AboutUsComponent,
+    OurServicesComponent,
+    MarqueeComponent,
+    ProjectsComponent,
+    TestimonialsComponent,
+    CtaComponent,
+  ],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
 })
-export class HomepageComponent {
-
-}
+export class HomepageComponent {}
