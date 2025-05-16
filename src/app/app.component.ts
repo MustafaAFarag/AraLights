f184ac3bbda75fc6669c7f509b4476556a2419ee
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-17 00:50:34 +0300

    Folder Structure and Layout Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { LucideAngularModule, FileIcon } from 'lucide-angular';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LucideAngularModule],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'aralights';
-  readonly FileIcon = FileIcon;
 }
