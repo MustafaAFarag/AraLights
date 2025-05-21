3fabf7c693cee60afd8397fbac590b5fc4a98cee
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-21 18:19:18 +0300

    Header Animation Done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +14,22 @@ import { Component, Input } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   @Input() title!: string;
+
+  @ViewChild('titleEl', { static: false }) titleEl!: ElementRef;
+
+  ngAfterViewInit() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from(this.titleEl.nativeElement, {
+      opacity: 0,
+      y: 50,
+      duration: 0.3,
+      scrollTrigger: {
+        trigger: this.titleEl.nativeElement,
+        once: true,
+      },
+    });
+  }
 }
