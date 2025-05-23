34c3ddc51fe19f87018b860dd3801ae96956dee1
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-23 21:04:03 +0300

    About us Page Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomepageComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
   },
 ];
