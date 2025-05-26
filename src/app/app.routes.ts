815d4509d7587ad24f04d5c11d5bb87a45b275df
Amr Yousre <amr.yousre@Adwat.local>
2025-05-26 14:06:41 +0300

    Projects Page Done and Responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'about-us',
     component: AboutUsComponent,
   },
+  {
+    path:"projects",
+    component:ProjectsComponent,
+  }
 ];
