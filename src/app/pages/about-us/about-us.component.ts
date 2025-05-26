c51cebb73ff6ef1f297142a212edd64e68fb03ff
Amr Yousre <amr.yousre@Adwat.local>
2025-05-26 14:47:03 +0300

    About us and Hero Section Video Added

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SectionHeaderComponent } from "../../components/section-header/section-header.component";
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [SectionHeaderComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SectionHeaderComponent } from "../../components/section-header/section-header.component";
 
 @Component({
   selector: 'app-projects',
-  imports: [CommonModule],
+  imports: [CommonModule, SectionHeaderComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
