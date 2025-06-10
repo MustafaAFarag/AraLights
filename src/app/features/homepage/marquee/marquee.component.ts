9eb539d8c73930f9c0506905f4fd852b8e37340d
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-06-10 14:22:24 +0300

    Marquee Localized

--- src/app/features/homepage/marquee/marquee.component.ts
+++ src/app/features/homepage/marquee/marquee.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../../services/language.service';
+
 @Component({
   selector: 'app-marquee',
   standalone: true,
+  imports: [CommonModule, TranslateModule],
   templateUrl: './marquee.component.html',
   styleUrls: ['./marquee.component.css'],
 })
-export class MarqueeComponent {}
+export class MarqueeComponent {
+  constructor(public languageService: LanguageService) {}
+}
