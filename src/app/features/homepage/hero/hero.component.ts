148b87eb1d363e6e789404fcdd4ee74c6a6433c4
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-06-10 13:34:36 +0300

    NGX vercel Problem Bug fixed

--- src/app/features/homepage/hero/hero.component.ts
+++ src/app/features/homepage/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, TranslateModule],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css'],
 })
