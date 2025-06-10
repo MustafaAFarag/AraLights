9525976e56b8e595a176ed49201f226b548da1c0
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-06-10 15:29:52 +0300

    Projects Page Done

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,22 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SectionHeaderComponent } from '../../components/section-header/section-header.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-projects',
-  imports: [CommonModule, SectionHeaderComponent],
+  imports: [CommonModule, SectionHeaderComponent, TranslateModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
   imgs = Array(3);
   items = [
-    'Interior',
-    'Interior',
-    'Interior',
-    'Interior',
-    'Interior',
-    'Interior',
+    'projects.filters.all',
+    'projects.filters.facade',
+    'projects.filters.interior',
+    'projects.filters.landscape',
+    'projects.filters.aviation',
   ];
   activeIndex = 0;
+
+  constructor(public languageService: LanguageService) {}
 }
