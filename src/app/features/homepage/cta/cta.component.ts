f6d880a4f48d691d9594adaa29e9a08a2ae54013
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-06-10 15:11:04 +0300

    Localization

--- src/app/features/homepage/cta/cta.component.ts
+++ src/app/features/homepage/cta/cta.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../../services/language.service';
 
 @Component({
   selector: 'app-cta',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, TranslateModule],
   templateUrl: './cta.component.html',
   styleUrls: ['./cta.component.css'],
 })
-export class CtaComponent {}
+export class CtaComponent {
+  constructor(public languageService: LanguageService) {}
+}
